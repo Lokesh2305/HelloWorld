cd2a228afd6da20d60daa081b5a9f945cace4e2a
Lokesh2305 <lokeshnaidu2305@gmail.com>
2021-09-28 22:57:42 -0700

    added HelloWorld program

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+  
+class Demo_print {
+    public static void main(String[] args)
+    {
+  
+        // using print()
+        // all are printed in the
+        // same line
+        System.out.print("GfG! ");
+        System.out.print("GfG! ");
+        System.out.print("GfG! ");
+    }
+}
